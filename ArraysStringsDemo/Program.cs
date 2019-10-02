016a5ab737c24a96fb9096231261435ad5abe129
student <student@PC_105_1->
2019-10-02 19:10:15 +0300

    arrays demo 1

--- ArraysStringsDemo/Program.cs
+++ ArraysStringsDemo/Program.cs
@@ -10,6 +10,35 @@ namespace ArraysStringsDemo
     {
         static void Main(string[] args)
         {
+            Object o = new Object();
+
+            Object o2 = null;
+            Object o3;
+
+            String s = "Hello";
+            string s2 = "Hello";
+            int[] intArray = { 1, 2, 3 };
+            int[] intArray2 = new int[3];
+
+            int[][] tDArray = new int[2][];
+
+
+            tDArray[0] = intArray;
+            tDArray[1] = intArray2;
+
+            foreach (var item in tDArray)
+            {
+                //Console.WriteLine(item);
+                foreach (var item2 in item)
+                {
+                    Console.Write(item2 + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            // String s1 = new String("Hello");
         }
     }
 }
